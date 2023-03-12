632794075d1f5b9e17088fbc7edd6f93130c4aa5
binblee <binblee@users.noreply.github.com>
2023-03-12 20:42:41 +0800

    version update

--- tf/aws/eks-tf-sample/main.tf
+++ tf/aws/eks-tf-sample/main.tf
@@ -21,7 +21,7 @@ provider "kubectl" {
 }
 
 module "eks_blueprints" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.21.0"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.25.0"
 
   cluster_name    = local.name
 
@@ -59,7 +59,7 @@ module "eks_blueprints" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.16.0"
+  version = "3.19.0"
 
   name = local.name
   cidr = local.vpc_cidr
